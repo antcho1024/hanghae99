daf6d48278ea076278a69038137bac9b93fb88d5
antcho1024 <50614113+antcho1024@users.noreply.github.com>
2022-09-05 13:21:19 +0900

    웹개발 종합반 3주차

--- sparta/pythonprac/hello.py
+++ sparta/pythonprac/hello.py
@@ -1,16 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
 
+from pymongo import MongoClient
+import certifi
+ca = certifi.where()
+client = MongoClient('mongodb+srv://test:sparta@cluster0.kafwra4.mongodb.net/Cluster0?retryWrites=true&w=majority', tlsCAFile=ca)
+db = client.dbsparta
+
 headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
 data = requests.get('https://movie.naver.com/movie/sdb/rank/rmovie.naver?sel=pnt&date=20210829',headers=headers)
 
 soup = BeautifulSoup(data.text, 'html.parser')
 
-# title = soup.select_one('#old_content > table > tbody > tr:nth-child(2) > td.title > div > a')
-# print(title['title'])
-
 movies = soup.select('#old_content > table > tbody > tr')
-# print(movies[1])
 
 for movie in movies:
     a = movie.select_one('td.title > div > a')
@@ -19,9 +21,9 @@ for movie in movies:
         rank = movie.select_one('td:nth-child(1) > img')['alt']
         star = movie.select_one('td.point').text
 
-        print(rank, title, star)
-
-#old_content > table > tbody > tr:nth-child(3) > td.title > div > a
-#old_content > table > tbody > tr:nth-child(2) > td.title > div > a
-#old_content > table > tbody > tr:nth-child(2) > td:nth-child(1) > img
-#old_content > table > tbody > tr:nth-child(2) > td.point
+        doc = {
+            'title': title,
+            'rank' : rank,
+            'star' : star
+        }
+        db.movies.insert_one(doc)
