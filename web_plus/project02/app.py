7c74710b125c955f5221365a8b7915faa963593a
hyeppy <50614113+antcho1024@users.noreply.github.com>
2022-09-19 19:10:25 +0900

    ajax 로 단어 상세페이지 만들기

---
+++ web_plus/project02/app.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, request, jsonify, redirect, url_for
+from pymongo import MongoClient
+import requests
+
+app = Flask(__name__)
+
+client = MongoClient('3.35.136.132', 27017, username="test", password="sparta")
+db = client.dbsparta_plus_week2
+
+
+@app.route('/')
+def main():
+    # DB에서 저장된 단어 찾아서 HTML에 나타내기
+    return render_template("index.html")
+
+
+@app.route('/detail/<keyword>')
+def detail(keyword):
+    # API에서 단어 뜻 찾아서 결과 보내기
+    r = requests.get(f"https://cros-anywhere.herokuapp.com/https://owlbot.info/api/v4/dictionary/{keyword}", headers={"Authorization": "Token 5d0c53cc965aa09d00853d0715a7bf17aea86f3f"})
+    result = r.json()
+    print(result)
+    return render_template("detail.html", word=keyword, result=result)
+
+
+@app.route('/api/save_word', methods=['POST'])
+def save_word():
+    # 단어 저장하기
+    return jsonify({'result': 'success', 'msg': '단어 저장'})
+
+
+@app.route('/api/delete_word', methods=['POST'])
+def delete_word():
+    # 단어 삭제하기
+    return jsonify({'result': 'success', 'msg': '단어 삭제'})
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
