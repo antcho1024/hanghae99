013e0f9efae723b3eea030420a20683f2f861a1c
hyeppy <50614113+antcho1024@users.noreply.github.com>
2022-09-23 12:50:50 +0900

    자바 프로그래머스 - 문자열을 정수로 바꾸기

--- java/chap01/Solution.java
+++ java/chap01/Solution.java
@@ -1,25 +1,17 @@
 import java.util.Scanner;
 public class Solution {
-    public long solution(long a, long b) {
-        long answer = 0;
-        if(a==b) return a;
-        else if (a>b) {
-            long tmp = a;
-            a= b;
-            b= tmp;
-        }
-        for(long i = a; i<=b ; i++){
-            answer +=i;
-        }
+    public int solution(String s) {
+        int answer = 0;
+        answer = Integer.parseInt(s);
         return answer;
     }
 
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        long a = sc.nextLong();
-        long b = sc.nextLong();
+        String s = sc.next();
         Solution sol = new Solution();
-        System.out.println(sol.solution(a,b));
+        System.out.println(sol.solution(s));
 
     }
 }
