286dfcf8bd543f3d924f8890b8fb9ab13cc7b306
hyeppy <50614113+antcho1024@users.noreply.github.com>
2022-09-28 15:26:43 +0900

    intelij Ultimate 버전으로 업그레이드

---
+++ java/chap01_2/Programmers_87389.java
@@ -0,0 +1,10 @@
+public class Programmers_87389 {
+    public int solution(int n) {
+        
+        int answer = 0;
+        return answer;
+    }
+    public static void main(String[] args){
+        System.out.println("hello");
+    }
+}
