a3e38a2e7f51eb2efafe9a35e9c3610168d344f0
hyeppy <50614113+antcho1024@users.noreply.github.com>
2022-10-05 13:18:30 +0900

    servlet vs controller 비교

--- Spring/week01_3/src/main/java/com/sparta/week01_3/Week013Application.java
+++ Spring/week01_3/src/main/java/com/sparta/week01_3/Week013Application.java
@@ -1,8 +1,11 @@
 package com.sparta.week01_3;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
+
 @ServletComponentScan // @WebServlet 어노테이션이 동작하게 함
 @SpringBootApplication
 public class Week013Application {
@@ -10,5 +13,11 @@ public class Week013Application {
     public static void main(String[] args) {
         SpringApplication.run(Week013Application.class, args);
     }
+    // Week02Application.java 의 main 함수 아래에 붙여주세요.
+    @Bean
+    public CommandLineRunner demo() {
+        return (args) -> {
 
+        };
+    }
 }
